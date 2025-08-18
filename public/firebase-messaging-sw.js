9c3607bf1999c4e3e6589a546ac2a9dd39239bea
ashabehr <ashabehrbakhshaee@yahoo.com>
2025-08-19 00:03:09 +0330

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,24 +1,29 @@
-importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js');
+// firebase-messaging-sw.js
+import { initializeApp } from "firebase/app";
+import { getMessaging, onBackgroundMessage } from "firebase/messaging/sw";
 
-firebase.initializeApp({
+const firebaseConfig = {
   apiKey: "AIzaSyBAePUfASka7W7TUuTe6iBfGF8PIn8Z_Wk",
   authDomain: "babarkat-dee4b.firebaseapp.com",
   projectId: "babarkat-dee4b",
   storageBucket: "babarkat-dee4b.firebasestorage.app",
   messagingSenderId: "826610864093",
-  appId: "1:826610864093:web:0895a974ec8681ac067034"
-});
+  appId: "1:826610864093:web:0895a974ec8681ac067034",
+};
 
-const messaging = firebase.messaging();
+const app = initializeApp(firebaseConfig);
+const messaging = getMessaging(app);
 
-messaging.onBackgroundMessage(function(payload) {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+onBackgroundMessage(messaging, (payload) => {
+  console.log(
+    "[firebase-messaging-sw.js] Received background message ",
+    payload
+  );
 
   const notificationTitle = payload.notification?.title || "Notification";
   const notificationOptions = {
     body: payload.notification?.body || "",
-    icon: '/plasmic/babarkat/images/image78.svg'
+    icon: "/plasmic/babarkat/images/image78.svg",
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
