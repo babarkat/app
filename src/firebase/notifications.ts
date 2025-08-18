01f4f40845061fa77f35a8664db77eb770efceb3
ashabehr <ashabehrbakhshaee@yahoo.com>
2025-08-18 22:17:01 +0330

    Create notifications.ts

---
+++ src/firebase/notifications.ts
@@ -0,0 +1,31 @@
+// notifications.ts
+import { messaging } from "./firebase";
+import { getToken, onMessage } from "firebase/messaging";
+
+export const requestPermission = async (): Promise<string | null> => {
+  try {
+    const permission = await Notification.requestPermission();
+    if (permission === "granted") {
+      const currentToken = await getToken(messaging, {
+        vapidKey: "BMf6ryVO3UrbQJ-_h-EFxHa3Gy8EQiwCOnmQGLFuY9W-POujf52qpJXiadtPYT2mhnQGxqQFvrmMYCqV5qMT5Ds",
+      });
+      const savedToken = localStorage.getItem("fcmToken");
+      console.log("Saved FCM Token:", savedToken);
+      return currentToken;
+    } else {
+      console.log("Permission not granted");
+      return null;
+    }
+  } catch (err) {
+    console.error("Error getting token:", err);
+    return null;
+  }
+};
+
+// دریافت پیام هنگام فعال بودن تب
+export const onMessageListener = (): Promise<any> =>
+  new Promise((resolve) => {
+    onMessage(messaging, (payload) => {
+      resolve(payload);
+    });
+  });
