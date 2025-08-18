a847a1d8ba7d59b1cb8bf6f3c35853b2f1170cbc
ashabehr <ashabehrbakhshaee@yahoo.com>
2025-08-18 22:32:38 +0330

    Create firebase-messaging-sw.js

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,25 @@
+importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBAePUfASka7W7TUuTe6iBfGF8PIn8Z_Wk",
+  authDomain: "babarkat-dee4b.firebaseapp.com",
+  projectId: "babarkat-dee4b",
+  storageBucket: "babarkat-dee4b.firebasestorage.app",
+  messagingSenderId: "826610864093",
+  appId: "1:826610864093:web:0895a974ec8681ac067034"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+
+  const notificationTitle = payload.notification?.title || "Notification";
+  const notificationOptions = {
+    body: payload.notification?.body || "",
+    icon: '/plasmic/babarkat/images/image78.svg'
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
